0e3459b05e114f123c246b0f7dbf90cae571d9e1
frankEatsNoodles <55frankwu@gmail.com>
2026-07-31 15:43:11 -0400

    Use new sql query to get active trips

--- src/main/java/com/transit/transitdata/controller/TransitDataAPIController.java
+++ src/main/java/com/transit/transitdata/controller/TransitDataAPIController.java
@@ -1,10 +1,14 @@
 package com.transit.transitdata.controller;
 
-import com.transit.transitdata.service.DBConnectionService;
+import com.transit.transitdata.service.DBSetupService;
+import com.transit.transitdata.service.DataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @RestController
 @RequestMapping("/setup/init/db/")
@@ -12,8 +16,10 @@ public class TransitDataAPIController {
 
 
     @Autowired
-    DBConnectionService dbConnection;
+    DBSetupService dbConnection;
 
+    @Autowired
+    DataService dataService;
 
     @GetMapping("/importStops")
     public String initdbImportStops() throws Exception {
--- src/main/java/com/transit/transitdata/service/DataService.java
+++ src/main/java/com/transit/transitdata/service/DataService.java
@@ -1,19 +1,41 @@
 package com.transit.transitdata.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 @Service
 public class DataService {
 
+    DateTimeFormatter dayFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+    DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     /**
      * Get the schedule routes at time from the db
+     *
+     * See resources/sql:
+     * SELECT COUNT(*) from __get_active_trips('85', '20260717', '14:30:00');
+     * returns the number of active scheduled trips
+     *
+     * @param routeId route id on db table
+     * @param date
+     * @param time
+     * @return
      */
-    public void dbRoutes(){
+    public Integer dbRoutes(String routeId, LocalDate date, LocalTime time){
 
-
+        return jdbcTemplate.queryForObject(
+                "SELECT COUNT(*) FROM __get_active_trips(?, ?, ?)",
+                Integer.class,
+                routeId, dayFormatter.format(date), timeFormatter.format(time)
+        );
 
     }
-
-
-
 }
